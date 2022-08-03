00c2193fc615cb26d42ec17e9000bf93c728448f
sayed.ahmed <sayed.ahmed@nextory.com>
2022-08-03 12:04:10 +0530

    Initial commit

--- src/main/java/samples/NestedLoopWithStream.java
+++ src/main/java/samples/NestedLoopWithStream.java
@@ -1,11 +1,37 @@
 package samples;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.System.*;
 
 public class NestedLoopWithStream {
-    static List<List<Integer>> list = List.of(List.of(1,2,3,4,5),List.of(11,12,13,14,15));
+
     public static void main(String[] args){
-        list.stream().flatMap(e->e.stream()).forEach(System.out::println);
+        findFirstExample();
     }
+
+    private static void findAnyExample() {
+        Integer[] empIds = {1,2,3,4};
+        Integer val = Stream.of(empIds)
+                .findAny()
+                .orElse(null);
+        out.println(val);
+    }
+
+    private static void findFirstExample() {
+        Integer[] empIds = {1,2,3,4};
+        Integer val = Stream.of(empIds)
+                .findFirst()
+                .orElse(null);
+        out.println(val);
+    }
+
+    private static void flatMapExample() {
+        List<List<Integer>> list = List.of(List.of(1,2,3,4,5),List.of(11,12,13,14,15));
+        list.stream().flatMap(Collection::stream).forEach(out::println);
+    }
+
 
 }
